8b09cc41fb89cf59b2180595993dca2ed4d2e0cc
alexangria1992 <alex040892@gmail.com>
2022-05-02 20:46:00 +1200

    fetch products from backend

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,12 @@ import data from "./data.js";
 
 const app = express();
 
-//test
+// test
 app.get("/api/products", (req, res) => {
-  res.send(data);
+  res.send(data.products);
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`server at http://localhost:${port}`);
+  console.log(`serve at http://localhost:${port}`);
 });
