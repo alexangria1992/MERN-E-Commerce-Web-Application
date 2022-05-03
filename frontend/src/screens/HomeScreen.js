cc0909873109b87fd83f6dd3429905eb3549a8dd
alexangria1992 <alex040892@gmail.com>
2022-05-03 16:32:10 +1200

    Add Bootstrap

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useReducer } from "react";
 import { Link } from "react-router-dom";
 // import data from "../data";
 import axios from "axios";
@@ -49,7 +49,7 @@ function HomeScreen() {
           products.map((product) => (
             <div key={product.slug} className="product">
               <Link to={`/product/${product.slug}`}>
-                <img src={product.image} />
+                <img src={product.image} alt={product.name} />
               </Link>
               <div className="product-info">
                 <Link to={`/product/${product.slug}`}>
