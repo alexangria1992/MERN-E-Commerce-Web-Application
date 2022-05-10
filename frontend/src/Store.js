1c083c4ee14295031ad20b9f4e425972a2c38824
alexangria1992 <alex040892@gmail.com>
2022-05-10 17:45:19 +1200

    Create Payment Screen

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -12,6 +12,10 @@ const initialState = {
       ? JSON.parse(localStorage.getItem("shippingAddress"))
       : {},
 
+    paymentMethod: localStorage.getItem("paymentMethod")
+      ? localStorage.getItem("paymentMethod")
+      : "",
+
     cartItems: localStorage.getItem("cartItems")
       ? JSON.parse(localStorage.getItem("cartItems"))
       : [],
@@ -52,6 +56,7 @@ function reducer(state, action) {
         cart: {
           cartItems: [],
           shippingAddress: {},
+          paymentMethod: "",
         },
       };
     case "SAVE_SHIPPING_ADDRESS":
@@ -60,6 +65,14 @@ function reducer(state, action) {
         cart: {
           ...state.cart,
           shippingAddress: action.payload,
+        },
+      };
+    case "SAVE_PAYMENT_METHOD":
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          paymentMethod: action.payload,
         },
       };
     default:
